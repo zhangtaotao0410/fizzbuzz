1c6f9fd97b37cab12b6c86f95c2e8728976673eb
zhangtaotao <zhangtaotao0410@gmail.com>
2020-05-16 16:39:48 +0800

    Default Changelist

--- src/test/java/com/tdd/FizzBuzzTest.java
+++ src/test/java/com/tdd/FizzBuzzTest.java
@@ -8,7 +8,9 @@ import static org.junit.Assert.assertThat;
 public class FizzBuzzTest {
     @Test
     public void should_print_num_when_input_is_normal_num() {
-        FizzBuzz fizzBuzz = new FizzBuzz(1);
-        assertThat(fizzBuzz.print(), is("1"));
+        FizzBuzz fizzBuzz1 = new FizzBuzz(1);
+        FizzBuzz fizzBuzz2 = new FizzBuzz(2);
+        assertThat(fizzBuzz1.print(), is("1"));
+        assertThat(fizzBuzz2.print(), is("2"));
     }
 }
